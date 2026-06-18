802fb1f4ecb39aad1198b99e3d78f4dc3c09fc7d
Oscar Vega <oscarvegaherrera59@gmail.com>
2026-06-18 18:16:25 +0200

    Custom Pipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,7 +1,23 @@
-import { Component } from "@angular/core";
+import { ToggleCasePipe } from './../../pipes/toggle-case.pipe';
+import { Component, signal } from "@angular/core";
 
 @Component({
-  templateUrl: './custom-page.component.html'
+  templateUrl: './custom-page.component.html',
+  imports: [ToggleCasePipe]
 })
 
-export default class CustomPageComponent {}
+export default class CustomPageComponent {
+
+  name = signal('Óscar vega herrera');
+
+  upper = signal(true);
+
+  changeUpperLower() {
+    if (this.upper()) {
+      return this.upper.set(false);
+    }
+
+    this.upper.set(true);
+  }
+
+}
